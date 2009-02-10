b2d6d6bbd70a5cbb4f4f32486001d2b12f7985d3
Witold Firlej <witek.firlej@gmail.com>
2009-02-10 22:33:09 +0100

    initial

---
+++ ircStat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#ircStat ver. 0.1 by grizz - Witek Firlej http://grizz.pl
+
+__name__      = "ircStat"
+__author__    = "Witold Firlej (http://grizz.pl)"
+__version__   = "0.1"
+__license__   = "GPL"
+__copyright__ = "Witold Firlej"
+
+def about ():
+    """About ircStat"""
+    print "______________"
+    print __name__ + " ver. " + __version__ + " by " + __author__
+    print ""
+    
+def main():
+    """main loop"""
+    about()
+    
+main()
